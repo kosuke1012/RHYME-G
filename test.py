87b48006f793e57dd4157d1d8e7777e18ae520e7
Miyamoto <scotsdowneroad@gmail.com>
2016-03-13 02:02:43 +0900

    update

--- test.py
+++ test.py
@@ -1,24 +1,43 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# -*- coding:utf-8 -*-
 
 import MeCab
-#import word_converter
 
-def StringJa2Katakana(String):
-    mecab = MeCab.Tagger()
-    text = String.encode('utf-8')
-    res =  mecab.parseToNode(text)
+### Constants
+MECAB_MODE = 'mecabrc'
+PARSE_TEXT_ENCODING = 'utf-8'
+
+### Functions
+def main():
+    sample_u = u"アイウエオ"
+    word_dict = parse(sample_u)
+    print word_dict
+    #print "Nouns:", ",".join(words_dict['nouns'])
+    #print "Verbs:", ",".join(words_dict['verbs'])
+    #print "Adjs:", ",".join(words_dict['adjs'])
+    return
+
+
+def parse(unicode_string):
+    tagger = MeCab.Tagger(MECAB_MODE)
+    # str 型じゃないと動作がおかしくなるので str 型に変換
+    text = unicode_string.encode(PARSE_TEXT_ENCODING)
+    node = tagger.parseToNode(text)
     
-    hira = ""
-    
-    while res:
-    #        print res.surface
-            ar = res.feature.split(",")
-            print ar
-            hira += ar[0]
-            res = res.next
-    return hira
+    words = []
+    nouns = []
+    verbs = []
+    adjs = []
+    pos = u""
+    while node:
+        st = node.feature.split(",")[8]
+        # unicode 型に戻す
+        word = node.surface.decode("utf-8")
+        pos += st.decode("utf-8")
+        node = node.next
 
+    return pos
 
-string = u"$B$"$$$&$($*(B"
-print StringJa2Katakana(string)
+### Execute
+if __name__ == "__main__":
+    main()
