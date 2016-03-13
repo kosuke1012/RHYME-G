6c2fdf8b49b07a27371a265299469e970642064c
kinowaki <kinowaki.y@gmail.com>
2016-03-14 00:52:23 +0900

    add test trim_text
    @Tweet2bot @ Tweet3bot ラップデータベース
    -> ラップデータベース

--- tweet2bot.py
+++ tweet2bot.py
@@ -1,5 +1,7 @@
 #-*-coding:utf-8-*-
 
+import re
+
 import twitter
 import twitkey
 import tweet2me as t2m
@@ -13,24 +15,32 @@ class Tweet2bot:
         ACCESS_TOKEN_KEY = twitkey.twkey['accto_key']
         ACCESS_TOKEN_SECRET = twitkey.twkey['accto_sec']
 
-        api = twitter.Api(consumer_key=CONSUMER_KEY,
+        self.api = twitter.Api(consumer_key=CONSUMER_KEY,
                           consumer_secret=CONSUMER_SECRET,
                           access_token_key=ACCESS_TOKEN_KEY,
                           access_token_secret=ACCESS_TOKEN_SECRET)
-        return api
+        
 
     def get_id_tweets2you_TL(self):
-        id_tweets2you = api.GetReplies(count=5)
+        id_tweets2you = self.api.GetReplies(count=5)
         #count=5 means last 5 tweets to you
         
         return id_tweets2you
 
     def get_id_tweets2me_TL(self):
-        id_tweets2me = api.GetReplies(count=5)
+        id_tweets2me = self.api.GetReplies(count=5)
         #count=5 means last 5 tweets to me
         
         return id_tweets2me
 
+    def get_id_tweet2you_DB(self):
+        #record from db
+        print ""
+
+    def get_id_tweet2me_DB(self):
+        #record from db
+        print ""
+    
     def notmatch_id(self, id_new, id_old):
         if len(id_old) < 0:
             return id_new
@@ -41,21 +51,55 @@ class Tweet2bot:
                     id_not_match.append(i)
             return id_notmatch
 
-    def set_id_tweet2me(self):
-        #record to DB
-        print ""
-
     def set_id_tweet2you(self):
         #record to DB
+        id_new = self.get_id_tweets2you_TL()
+        id_old = self.get_id_tweet2you_DB()
+        id_notmatch = self.notmatch_id(id_new, id_old)
         print ""
-        
-    def set_text2me(self):
+
+    def set_id_tweet2me(self):
         #record to DB
+        id_new = self.get_id_tweets2me_TL()
+        id_old = self.get_id_tweet2me_DB()
+        id_notmatch = self.notmatch_id(id_new, id_old)
         print ""
 
     def set_text2you(self):
         #record to DB
         print ""
+
+    def set_text2me(self):
+        #record to DB
+        print ""
+    
+    def trim_text(self,text):
+        at_in_text = re.findall('@.*\s', text)
+        print at_in_text
+        if len(at_in_text) > 0:
+            for at_ in at_in_text:
+                trim_text = text.strip(at_)
+        return trim_text
+
+    def run(self):
+        #get tweet
+        
+        #compare tweet with DB
+
+        #text2me -> text2you (Word Converter)
+        
+        #tweet2you
+
+        #Record to DB
+
+        print ""
+    
+def test():
+    t2b = Tweet2bot()
+    print t2b.trim_text('@Tweet2bot @ Tweet3bot ラップデータベース')
+
+if __name__ == '__main__':
+    test()
 
     
     
