e8103b5eef6ad21ae542745f751d28452b38595a
Miyamoto <scotsdowneroad@gmail.com>
2016-03-18 22:48:38 +0900

    add test of sentence_word

--- sentence2word.py
+++ sentence2word.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 import MeCab
 
+
 ### Constants
 MECAB_MODE = 'mecabrc'
 PARSE_TEXT_ENCODING = 'utf-8'
@@ -21,8 +22,18 @@ def sentence_word(sentence):
         pos = st[0]
         if pos==d or pos==m or pos==k :
             word = node.surface.decode("utf-8")
-            print word
+            #print word
             word_list.append(word)
 
         node = node.next
     return word_list
+
+
+if __name__=='__main__':
+    import WordConverter 
+    print 'test'
+    sentence = "川俣軍司スタイルぶたぎる通り魔スタイル"
+    word = sentence_word(sentence)
+    for x in word:
+        print x
+        print WordConverter.nihongo_boin(x) 
